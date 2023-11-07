9db9d85c9ee7aa65364fa6ce7e67e475ce861751
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-11-07 20:29:19 +0530

    Update attributes.py

--- attributes.py
+++ attributes.py
@@ -1,8 +1,8 @@
 import numpy as np
 array3=np.array([[2,4.3],[4.91,7],[0,-1]])
 array3
-array3.ndim  #dimentions
-array3.dtype  # common data types like int32 int64 flot 32 float63 <u>
-array3.shape   #sequence of integers indicate the size of the array
-array3.size    #total elementrs or  equal to product  of shape
-array3.dtypesize  #dtype size like 4byte or 8byte or 128 
+print(array3.ndim)  #dimentions
+print(array3.dtype)  # common data types like int32 int64 flot 32 float63 <u>
+print(array3.shape)   #sequence of integers indicate the size of the array
+print(array3.size)    #total elementrs or  equal to product  of shape
+print(array3.itemsize ) #dtype size like 4byte or 8byte or 128 
