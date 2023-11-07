6afd3d6963020311c00e4527d71543f2399e5053
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-11-07 20:13:28 +0530

    Create slicing2.py

---
+++ slicing2.py
@@ -0,0 +1,4 @@
+array10=np.array([[-7,0,10,20,],[-5,1,40,200],[-1,1,4,30]])
+#access all the elements in the 3 rd column 
+#acess elements of 2 nd and 3 rd from 1 st and 2 nd column
+array10[1:3,0:2]
